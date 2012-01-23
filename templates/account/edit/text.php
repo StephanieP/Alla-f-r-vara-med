c11b54da3cb4d7c906a2b3b3d01c851c50301f75
Felix Stridsberg <felixstridsberg@gmail.com>
2012-01-23 17:38:53 +0100

    Enbart designen på profilen

--- templates/account/edit/text.php
+++ templates/account/edit/text.php
@@ -4,16 +4,34 @@
 	if (!defined("_EXECUTE")) { echo "Not allowed"; exit;}
 ?>
 
-<h1><?php echo $template->profile_username; ?></h1>
+
 <article>
-	<p>
-    	<form action="" method="post">
-            <textarea name="profile_text"><?php 
-                if ($template->profile_text != null):
-                    echo $template->profile_text;
-                endif;
-            ?></textarea><br>
-            <input type="submit" value="Spara" />
-        </form>
-	</p>
+    	<div class="user_profile_menu">
+        	<div class="user_profile_image">
+            
+            </div>
+            <div>
+            	<nav>
+                    <ul>
+                        <li>
+                            <a href="/account/">Tillbaka till profilen</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        
+		<div class="user_profile_content">
+        	<h1><?php echo $template->profile_username; ?></h1>
+            <form action="" method="post">
+                <textarea name="profile_text" style="width: 405px"><?php 
+                    if ($template->profile_text != null):
+                        echo $template->profile_text;
+                    endif;
+                ?></textarea><br>
+                <input type="submit" value="Spara" />
+            </form>
+        </div>
+        <div style="clear: both;"></div>
+        
 </article>
--- templates/account/profile.php
+++ templates/account/profile.php
@@ -4,38 +4,58 @@
 	if (!defined("_EXECUTE")) { echo "Not allowed"; exit;}
 ?>
 
-<h1><?php echo $template->profile_username; ?></h1>
 <article>
 
 	<?php 
-		// Existerar inte användarnamnet finns ej användaren
-		if ($template->profile_username == null): ?>
-        <p>
-        	Användaren finns icke. =(
-        </p>
-    <?php 
-		// Om profiltext finns, skriv ut den
-		elseif ($template->profile_text != null): ?>
-        <p>
-        	<?php echo nl2br($template->profile_text); ?>
-        </p>
-    <?php
-		// Om profiltext inte finns skriv ut detta
-		else: ?>
-        <p>
-        	<?php echo $template->profile_username; ?> har inte skrivit något ännu...<br />
-			Buhu ='(
-        </p>
-    <?php endif; ?>
-
-
-    
+		// Existerar användarnamnet finnsanvändaren
+		if ($template->profile_username != null): ?>
+	
+    	<div class="user_profile_menu">
+        	<div class="user_profile_image">
+            
+            </div>
+            <div>
+            	<nav>
+                    <ul>
+                     <?php
+                        // Om användaren är på sin egen profil
+                        if ($template->is_profile_admin): ?>
+                        <li>
+                            <a href="/account/edit/text/">Ändra text</a>
+                        </li>
+                    <?php endif; ?>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    	
+		<div class="user_profile_content">
+        	<h1><?php echo $template->profile_username; ?></h1>
+			<?php 
+                // Om profiltext finns, skriv ut den
+                if ($template->profile_text != null): ?>
+                <p>
+                    <?php echo nl2br($template->profile_text); ?>
+                </p>
+            <?php
+                // Om profiltext inte finns skriv ut detta
+                else: ?>
+                <p>
+                    <?php echo $template->profile_username; ?> har inte skrivit något ännu...<br />
+                    Buhu ='(
+                </p>
+            <?php endif; ?>
+        
+        
+ 
+        </div>
+        <div style="clear: both;"></div>
+        
 	<?php
-		// Om användaren är på sin egen profil
-		if ($template->is_profile_admin): ?>
-        <p>
-            <a href="/account/edit/text/">Ändra innehåll</a>
-        </p>
+		//Användaren finns ej
+		else: ?>
+            <p>
+            	Användaren finns icke. =(
+            </p>
     <?php endif; ?>
-
 </article>
