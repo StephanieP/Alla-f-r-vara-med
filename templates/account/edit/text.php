751eeb9187ca50a3940edb60543a7f0f76b3201c
Felix Stridsberg <felixstridsberg@gmail.com>
2012-01-23 18:36:54 +0100

    Headerbild

--- templates/account/edit/text.php
+++ templates/account/edit/text.php
@@ -4,34 +4,33 @@
 	if (!defined("_EXECUTE")) { echo "Not allowed"; exit;}
 ?>
 
-
-<article>
-    	<div class="user_profile_menu">
-        	<div class="user_profile_image">
-            
-            </div>
-            <div>
-            	<nav>
-                    <ul>
-                        <li>
-                            <a href="/account/">Tillbaka till profilen</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
+<aside>
+    <div class="user_profile_menu">
+        <div class="user_profile_image">
         
-		<div class="user_profile_content">
-        	<h1><?php echo $template->profile_username; ?></h1>
-            <form action="" method="post">
-                <textarea name="profile_text" style="width: 405px"><?php 
-                    if ($template->profile_text != null):
-                        echo $template->profile_text;
-                    endif;
-                ?></textarea><br>
-                <input type="submit" value="Spara" />
-            </form>
         </div>
-        <div style="clear: both;"></div>
-        
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/account/">Tillbaka till profilen</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</aside>
+<article>        
+    <div class="user_profile_content">
+        <h1><?php echo $template->profile_username; ?></h1>
+        <form action="" method="post">
+            <textarea name="profile_text" style="width: 405px"><?php 
+                if ($template->profile_text != null):
+                    echo $template->profile_text;
+                endif;
+            ?></textarea><br>
+            <input type="submit" value="Spara" />
+        </form>
+    </div>
+    <div style="clear: both;"></div>
 </article>
